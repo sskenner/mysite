6d9365dca978864d2f214080d491c85794629cf4
santana kenner <sskenner@gmail.com>
2018-04-07 21:27:42 +0000

    update host info for c9

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',  default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = ['127.0.0.1','mysites-sskenner.c9users.io']
 
 # Application definition
 
